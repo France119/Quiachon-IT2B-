0cd0341a4ed5309d2c14983c175afac88a7f71b6
France119 <quiachonfrancis@gmail.com>
2024-09-19 08:21:28 +0800

    First push

---
+++ src/javaquiachon/Reciept.java
@@ -0,0 +1,40 @@
+
+package javaquiachon;
+
+
+public class Reciept {
+     String fname, pname;
+    int quan, price, cash;
+    
+    public void processReceipt(String fullname,String productname, int Quantity, int Price, int Cash){
+        
+        this.fname = fullname;
+        this.pname = productname;
+        this.quan = Quantity;
+        this.price = Price;
+        this.cash = Cash;
+        
+
+    }
+    public void viewReceipt(){
+        System.out.println("________________________");
+        System.out.println("Receipt");
+        System.out.println("________________________");
+        System.out.println("Name:"+fname);
+        System.out.println("Item:"+pname);
+        System.out.println("Quantity"+quan);
+        System.out.println("________________________");
+        System.out.println("Total due:"+ (quan*price));
+        
+        
+        System.out.println("Cash:"+cash);
+        System.out.println("________________________");
+        System.out.println("Change:"+(cash-(quan*price)));
+        System.out.println("________________________");
+        
+    }
+
+    void getreceipt() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
